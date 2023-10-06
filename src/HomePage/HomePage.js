9df2faed6edb0e5f8d6d9cdafe3d12bc55b1d4c2
rpm-1990 <reddypm1990@gmail.com>
2023-10-06 14:40:47 -0400

    Implemented D3chart

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,170 +1,89 @@
-import React, { useEffect, useState,useRef  } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import { Chart } from 'chart.js/auto';
 import * as d3 from 'd3';
-import { select } from 'd3';
 
 function Homepage() {
-   const [datasource] = useState(
-    {
-    //datasource :{
-                datasets: [
-                    {
-                      data: [],
-                      backgroundcolor: [
-                        '#ffcd56',
-                        '#ff0000',
-                        '#0000ff',
-                        '#4d5791',
-                        '#a52a2a',
-                        '#8a2be2',
-                        '#ffebcd',
-                        '#deb887',
-                        
-                      ],
+  const [datasource,setDatasource] = useState({
+    datasets: [
+      {
+        data: [],
+        backgroundColor: [
+        '#ffcd56', '#ff0000', '#0000ff', '#4d5791', '#a52a2a', '#8a2be2', '#ffebcd'
+        ],
+      },
+    ],
+    labels: [],
+  });
 
-                    },
-                ],
+  let myChartRef = useRef(null);
 
-               labels: [],
-            });
-    
-    const createChart = () => {
+  const createChart = () => {
+    if (myChartRef.current) {
+      myChartRef.current.destroy(); // Destroy existing Chart if it exists
+    }
     const ctx = document.getElementById("myChart").getContext("2d");
-    const myPieChart = new Chart(ctx, {
+    myChartRef.current = new Chart(ctx, {
       type: 'pie',
       data: datasource
- });
-}
+    });
+  }
 
+const createD3Chart = () => {
+  const width = 300;
+  const height = 300;
+  const radius = Math.min(width, height) / 2;
 
-  const createD3Chart = () => {
-    const width = 960;
-    const height = 450;
-    const radius = Math.min(width, height) / 2;
-    
-    const svg = select('#d3chart')
-      .append('svg')
-      .attr('width', 960)
-      .attr('height', 450)
-      .append('g')
-      .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
-  svg.append('g').attr('class', 'slices');
-    svg.append('g').attr('class', 'labels');
-    svg.append('g').attr('class', 'lines');
+  const color = d3.scaleOrdinal()
+  .domain(datasource.labels.map(label => label.toString()))
+  .range(['#ffcd56', '#ff0000', '#0000ff', '#4d5791', '#a52a2a', '#8a2be2', '#ffebcd']);
 
-    
+  const svg = d3.select('#d3Chart')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+    .attr('transform', `translate(${width / 2},${height / 2})`);
 
-    const pie= d3.pie().sort(null).value(d => d.value);
+  const arc = d3.arc()
+    .innerRadius(radius - 70)
+    .outerRadius(radius);
 
-    const arc= d3.arc()
-      .outerRadius(radius * 0.8)
-      .innerRadius(radius * 0.4);
+  const pie = d3.pie()
+    .value((d) => d)
+    .sort(null);
 
-    const outerArc = d3.arc()
-      .innerRadius(radius * 0.9)
-      .outerRadius(radius * 0.9);
+  const data = pie(datasource.datasets[0].data);
 
-    svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+  const path = svg.selectAll('path')
+    .data(data)
+    .enter()
+    .append('path')
+    .attr('d', (d) => arc(d)) // <-- Cast to string
+    .attr('fill', (d) => color(d.data.toString())); // <-- Convert to string
 
-    const key = (d) => d.data.label;
+  // Add labels with polylines
+  const labelArc = d3.arc()
+    .innerRadius(radius - 40)
+    .outerRadius(radius - 40);
 
-    const color = d3.scaleLinear()
-      .domain(["Electricity Bill", "dolor sit", "amet", "consectetur", "adipisicing", "elit", "sed", "do", "eiusmod", "tempor", "incididunt"])
-      .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
-    axios.get('/budget').then(res => {
-      const data = res.data.myBudget.map(obj => {
-        return { label: obj.title, value: obj.budget }
-      })
-      change(data);
-    })
-
-    function change(data) {
-      const slice = svg.select(".slices").selectAll("path.slice")
-        .data(pie(data), key);
-
-      slice.enter()
-        .insert("path")
-        .style("fill", d => color(d.data.label))
-        .attr("class", "slice");
-
-      slice.transition().duration(1000)
-        .attrTween("d", function (d) {
-          this._current = this._current || d;
-          const interpolate = d3.interpolate(this._current, d);
-          this._current = interpolate(0);
-          return function (t) {
-            return arc(interpolate(t));
-          };
-        })
-
-      slice.exit()
-        .remove();
-
-      const text = svg.select(".labels").selectAll("text")
-        .data(pie(data), key);
-
-      text.enter()
-        .append("text")
-        .attr("dy", ".35em")
-        .text(d => d.data.label);
-
-      function midAngle(d) {
-        return d.startAngle + (d.endAngle - d.startAngle) / 2;
-      }
-
-      text.transition().duration(1000)
-        .attrTween("transform", function (d) {
-          this._current = this._current || d;
-          const interpolate = d3.interpolate(this._current, d);
-          this._current = interpolate(0);
-          return function (t) {
-            const d2 = interpolate(t);
-            const pos = outerArc.centroid(d2);
-            pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
-            return "translate(" + pos + ")";
-          };
-        })
-        .styleTween("text-anchor", function (d) {
-          this._current = this._current || d;
-          const interpolate = d3.interpolate(this._current, d);
-          this._current = interpolate(0);
-          return function (t) {
-            const d2 = interpolate(t);
-            return midAngle(d2) < Math.PI ? "start" : "end";
-          };
-        });
-
-      text.exit()
-        .remove();
-
-      const polyline = svg.select(".lines").selectAll("polyline")
-        .data(pie(data), key);
-
-      polyline.enter()
-        .append("polyline");
-
-      polyline.transition().duration(1000)
-        .attrTween("points", function (d) {
-          this._current = this._current || d;
-          const interpolate = d3.interpolate(this._current, d);
-          this._current = interpolate(0);
-          return function (t) {
-            const d2 = interpolate(t);
-            const pos = outerArc.centroid(d2);
-            pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
-            return [arc.centroid(d2), outerArc.centroid(d2), pos];
-          };
-        });
-
-      polyline.exit()
-        .remove();
-    }
-}
-
-
-
+  const labels = svg.selectAll('text')
+    .data(data)
+    .enter()
+    .append('text')
+    .attr('transform', (d) => `translate(${labelArc.centroid(d)})`)
+    .attr('dy', '.35em')
+    .attr('text-anchor', 'middle')
+    .text(d => d.data);
+  const polyline = svg.selectAll('polyline')
+  .data(data)
+  .enter()
+  .append('polyline')
+  .attr('points', (d) => {
+    const pos = labelArc.centroid(d);
+    return [arc.centroid(d), labelArc.centroid(d), pos].join(' ');
+  });
+} 
     const getBudget = () => {
       axios.get('http://localhost:3000/budget')
         .then(function (res) {
@@ -172,6 +91,7 @@ function Homepage() {
             datasource.datasets[0].data[i] = res.data.myBudget[i].budget;
             datasource.labels[i] = res.data.myBudget[i].title;
           }
+          setDatasource({ ...datasource });
           createChart();
           createD3Chart();
         });
@@ -248,19 +168,16 @@ function Homepage() {
         </div>
         <div className="text-box">
             <h1>ChartJs</h1>
-           {/* {datasource.datasets[0].data.length === 0 ? <p>Loading chart data...</p> : <Pie data={datasource} />} */}
         <canvas id="myChart" width="400" height="400"></canvas>
         </div>        
-        <div className="text-box">
-            <h1>D3ChartJs</h1>
-           {/* {datasource.datasets[0].data.length === 0 ? <p>Loading chart data...</p> : <Pie data={datasource} />} */}
-       <svg width="960" height="450"></svg>;
-        </div>
+        <article>
+          <h1>D3 Chart</h1>
+              <div id="d3Chart"></div>
+      </article>
 
 
     </div>
 </div>
   );
 } 
-
-export default Homepage;
+export default Homepage
